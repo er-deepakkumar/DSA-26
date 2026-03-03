1f8fd650a947b51e779cb98a0b5ffb622e1bae50
Deepak Kumar <purbeydeepak005@gmail.com>
2026-03-03 18:04:06 +0530

    Factor of N

--- Week1/factor.cpp
+++ Week1/factor.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 using namespace std;
-int main()
-printf(
-    "jdlfasjdf"
-)
+int main(){
+    long long n,f;
+    cin>>n >>f;
+    if(n%f==0){
+        cout<<"Yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+}
