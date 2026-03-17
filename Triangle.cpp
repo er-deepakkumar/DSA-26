b4cf75daf9b4c340dce7a8d6d5d52fd49ac7a0b3
Deepak Kumar <purbeydeepak005@gmail.com>
2026-03-17 23:07:25 +0530

    pending

---
+++ Triangle.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+    long n;
+    if(!(cin>>n))return 0;
+    for(int i)
+}
--- Week2/tempCodeRunnerFile.cpp
+++ Week2/tempCodeRunnerFile.cpp
@@ -1,10 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    long long n;
-    cin>>n;
-    while(n!=0){
-        cout<<n%10;
-        n=n/10;
+
+int main() {
+    int n;
+    if (!(cin >> n)) return 0;
+
+    // Top half (increasing)
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            // j == 1 is the vertical left wall
+            // j == i is the slanted right wall
+            if (j == 1 || j == i) {
+                cout << "* ";
+            } else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
     }
+
+    // Bottom half (decreasing)
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            // Same logic: only print stars at the boundaries
+            if (j == 1 || j == i) {
+                cout << "* ";
+            } else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
