c1e9e640b7d43f6ea51322c92c3e54a16b2b68ff
DEEPAK KUMAR <purbeydeepak005@gmail.com>
2026-04-09 15:56:00 +0530

    ok

---
+++ Week2/apnaCollege/square.cpp
@@ -0,0 +1,73 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=4;
+//     for(int i=0;i<n;i++){
+//         int num=1;
+//         for(int j=0;j<n;j++){
+//             cout<<num<<" ";
+//             num+=1;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=4;
+//     for(int i=0;i<n;i++){
+        
+//         for(int j=1;j<=n;j++){
+//             cout<<"* ";
+            
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=4;
+//     for(int i=0;i<n;i++){
+//         char ch='A';
+//         for(int j=1;j<=n;j++){
+//             cout<<ch<<" ";
+//             ch+=1;
+            
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n=4;
+//     int num=1;
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n;j++){
+//             cout<<num<<" ";
+//             num+=1;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+#include <iostream>
+using namespace std;
+#include <iomanip>   // needed for setw  Full form → Input Output Manipulation    setw-> set width
+
+int main() {
+    int n = 4;
+    int num = 1;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout <<setw(n)<<num;
+            num += 1;
+        }
+        cout << endl;
+    }
+}
