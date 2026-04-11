f631c4e3053e780e8b1fa21cc63716c9f459a7e4
DEEPAK KUMAR <purbeydeepak005@gmail.com>
2026-04-11 17:04:41 +0530

    fg

--- Week2/apnaCollege/square.cpp
+++ Week2/apnaCollege/square.cpp
@@ -55,19 +55,34 @@
 //     }
 // }
 
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+// #include <iomanip>   // needed for setw  Full form → Input Output Manipulation    setw-> set width
+
+// int main() {
+//     int n = 4;
+//     int num = 1;
+
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < n; j++) {
+//             cout <<setw(n)<<num;
+//             num += 1;
+//         }
+//         cout << endl;
+//     }
+// }
+
+#include<iostream>
+#include<iomanip>
 using namespace std;
-#include <iomanip>   // needed for setw  Full form → Input Output Manipulation    setw-> set width
-
-int main() {
-    int n = 4;
-    int num = 1;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout <<setw(n)<<num;
-            num += 1;
+int main(){
+    int n=4;
+    char ch='A';
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            cout<<setw(n)<<ch;
+            ch+=1;
         }
-        cout << endl;
+        cout<<endl;
     }
 }
