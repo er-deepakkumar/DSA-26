2dcf1890d6c83d5aa07f1d92ef103be499c0b5e3
DEEPAK KUMAR <purbeydeepak005@gmail.com>
2026-04-18 16:52:20 +0530

    binary

---
+++ BinaryNumber/decToBinary.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int decNum;
+    cin>>decNum;
+    int ans=0;
+    int power=1;
+    while (decNum>0)
+    {
+        int rem=decNum%2;
+        decNum=decNum/2;
+        ans+=(rem*power);
+        power*=10;
+        
+    }
+    cout<<ans;
+    return 0;
+    
+}
