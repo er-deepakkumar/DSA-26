4c53d15145c6b587261a670ffe7a57af66ec55ee
Deepak Kumar <purbeydeepak005@gmail.com>
2026-05-06 23:22:59 +0530

    bitwize operator

---
+++ Bitwise_operation_AP_L7/bitwize.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=13, b=5;
+    // cout<<(a^b);
+
+    // bitwize and (&)
+    // bitwize or (|)
+    // bitwize xor (^)
+    // bitwize << (left shift operator)
+    // cout<<(4<<1);
+
+    // bitwize >> (right shift operator)
+    cout<<(4>>1);
+
+}
